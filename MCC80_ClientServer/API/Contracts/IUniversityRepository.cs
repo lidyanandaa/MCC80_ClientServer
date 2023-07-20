d0fe6c9369ec7959347b05ee4e708b7a6036adcf
Lidya Ananda Arsyanti <Lidyanandaars@gmail.com>
2023-07-20 12:03:52 +0700

    Update University

--- MCC80_ClientServer/API/Contracts/IUniversityRepository.cs
+++ MCC80_ClientServer/API/Contracts/IUniversityRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace API.Contracts
+﻿using API.Models;
+
+namespace API.Contracts
 {
-    public class IUniversityRepository
+    public interface IUniversityRepository
     {
+        IEnumerable<University> GetAll();
+        University? GetByGuid(Guid guid);
+        University? Create(University university);
+        bool Update(University university);
+        bool Delete(University university);
     }
 }
--- MCC80_ClientServer/API/Program.cs
+++ MCC80_ClientServer/API/Program.cs
@@ -1,5 +1,7 @@
+using API.Contracts;
 using API.Data;
 using API.Models;
+using API.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +12,9 @@ builder.Services.AddControllers();
 // Add dbConnection to the container.
 var connection = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<BookingDbContext>(option => option.UseSqlServer(connection));
+
+// Add repositories to the container.
+builder.Services.AddScoped<IUniversityRepository, UniversityRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();

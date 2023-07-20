03cf4281b8592fa25a41cf1face4ea085717d002
Lidya Ananda Arsyanti <Lidyanandaars@gmail.com>
2023-07-20 16:22:05 +0700

    Update Booking AccuountRole Account

--- MCC80_ClientServer/API/Program.cs
+++ MCC80_ClientServer/API/Program.cs
@@ -19,6 +19,9 @@ builder.Services.AddScoped<IRoomRepository, RoomRepository>();
 builder.Services.AddScoped<IRoleRepository, RoleRepository>();
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 builder.Services.AddScoped<IEducationRepository, EducationRepository>();
+builder.Services.AddScoped<IBookingRepository, BookingRepository>();
+builder.Services.AddScoped<IAccountRoleRepository, AccountRoleRepository>();
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();

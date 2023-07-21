3239ee5c5da7e7130ccf78285567f7c5ff6b9653
Lidya Ananda Arsyanti <Lidyanandaars@gmail.com>
2023-07-21 10:46:44 +0700

    Update General

--- MCC80_ClientServer/API/Repositories/RoomRepository.cs
+++ MCC80_ClientServer/API/Repositories/RoomRepository.cs
@@ -5,77 +5,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Repositories
 {
-    public class RoomRepository : IRoomRepository
+    public class RoomRepository : GeneralRepository<Room>, IRoomRepository
     {
-        private readonly BookingDbContext _context;
-
-        public RoomRepository(BookingDbContext context)
+        public RoomRepository(BookingDbContext context) : base(context)
         {
-            _context = context;
-        }
 
-        //GETALL ROOM
-        public IEnumerable<Room> GetAll()
-        {
-            return _context.Set<Room>()
-                           .ToList();
-        }
-
-        //GETBYID ROOM
-        public Room? GetByGuid(Guid guid)
-        {
-            var data = _context.Set<Room>()
-                               .Find(guid);
-            _context.ChangeTracker.Clear();
-            return data;
-        }
-
-        //CREATE ROOM
-        public Room? Create(Room room)
-        {
-            try
-            {
-                _context.Set<Room>()
-                        .Add(room);
-                _context.SaveChanges();
-                return room;
-            }
-            catch
-            {
-                return null;
-            }
-        }
-
-        //UPDATE ROOM
-        public bool Update(Room room)
-        {
-            try
-            {
-                _context.Entry(room)
-                        .State = EntityState.Modified;
-                _context.SaveChanges();
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-        }
-
-        //DELETE ROOM
-        public bool Delete(Room room)
-        {
-            try
-            {
-                _context.Set<Room>()
-                        .Remove(room);
-                _context.SaveChanges();
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
         }
     }
 }

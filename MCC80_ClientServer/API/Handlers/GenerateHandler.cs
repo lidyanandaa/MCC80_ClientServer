fb02955f9168afa4d50acb663ed8cefcb149611e
Lidya Ananda Arsyanti <Lidyanandaars@gmail.com>
2023-07-25 09:15:12 +0700

    Update

--- MCC80_ClientServer/API/Handlers/GenerateHandler.cs
+++ MCC80_ClientServer/API/Handlers/GenerateHandler.cs
@@ -2,15 +2,29 @@
 {
     public class GenerateHandler
     {
-        public static string Nik(string? nik=null)
+        public static string Nik(string? nik = null)
         {
-        if (nik == null)
+            /* Random random = new Random();
+
+             // Generate random digits for the NIK (assumes a 16-digit NIK).
+             int[] nikDigits = new int[16];
+             for (int i = 0; i < 16; i++)
+             {
+                 nikDigits[i] = random.Next(6); // Generates a random digit between 0 and 9.
+             }
+
+             // Format the NIK (Indonesia's NIK might have specific rules for each region).
+             string formattedNIK = string.Join("", nikDigits);
+
+             return formattedNIK;*/
+
+            if (nik == null)
             {
                 return "111111";
             }
-        
+
             var generatedNik = int.Parse(nik) + 1;
-        
+
             return generatedNik.ToString();
         }
     }
